bc00b4cad6a9a09616dce761c4e720df36ec7f7f
Pazu Cheung <pforpazu@gmail.com>
2022-11-11 20:26:10 -0700

    cancel unused import

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
 import { createRoot } from "react-dom/client";
